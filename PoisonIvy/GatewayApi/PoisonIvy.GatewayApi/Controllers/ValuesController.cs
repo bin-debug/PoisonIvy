b53c091845e4a001c6a3d5fceac61b170ef64f8a
Nivash Singh <nivashsingh@gmail.com>
2019-11-13 18:24:22 +0200

    added some dummy controllers to get started with the gateway

---
+++ PoisonIvy/GatewayApi/PoisonIvy.GatewayApi/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PoisonIvy.GatewayApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Gateway API :)");
+        }
+    }
+}
---
+++ PoisonIvy/Services/Basket/PoisonIvy.BasketApi/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PoisonIvy.BasketApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Basket API :)");
+        }
+    }
+}
